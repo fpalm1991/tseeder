54a5001d49618ad65a9dcbc4f48ad5be5839bff5
Feliu <felix1991palm@gmail.com>
2026-06-19 12:30:14 +0200

    Refactor

--- assets/js/controllers/tseeder_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-export default class extends Controller {
-    static targets = ["name", "output"];
-
-    greet() {
-        this.outputTarget.textContent = `Welcome, ${this.nameTarget.value}!`;
-    }
-}
